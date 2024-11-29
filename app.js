7fc517061f372271f35fafea5aacd893186285dc
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-11-29 14:58:05 -0800

    answer to exercise 2

--- app.js
+++ app.js
@@ -19,3 +19,33 @@ const newArr = nums.map( num => {
 })
 
 console.log(newArr)
+
+
+const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
+
+const [firstPet, secondPet] = petsArray;
+
+console.log(firstPet); // 'Rover'
+console.log(secondPet); // 'Snuffles'
+
+// Equivalent in traditional bracket notation:
+console.log(petsArray[0]); // 'Rover'
+console.log(petsArray[1]); // 'Snuffles'
+
+//Exercise 2: Array destructuring
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const [firstFruit, secondFruit] = pizzaToppings
+
+console.log(firstFruit)
+console.log(secondFruit)
+
+const person = {
+    name: 'Alex',
+    role: 'Software Engineer',
+  };
+  
